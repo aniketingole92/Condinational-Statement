5bd2f9dbc8edffc711dcf277e0d7255a0864bdb2
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-03-27 11:04:03 +0530

    Create if.java

---
+++ if.java
@@ -0,0 +1,16 @@
+class hemant 
+{
+	public static void main(String [] args)
+	{
+		int token = 50;
+		if (token == 30){
+			System.out.println("Collect in water bottel token");
+		} else if (token == 40){
+			System.out.println("collect water token");
+		} else if (token == 50 ){
+			System.out.println("Collect the token");
+		} else {
+			System.out.println("Invalid token");
+		}
+	}
+}
