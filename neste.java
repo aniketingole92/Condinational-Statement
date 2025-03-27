3e8b0b100c574b79673f35f8ce4513f62b938989
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-03-27 12:49:53 +0530

    Create neste.java

---
+++ neste.java
@@ -0,0 +1,15 @@
+class nestt
+{
+	public static void main(String [] args)
+	{
+		int token = 30;
+		char softDrink = 'S';
+		if (token == 30){
+			if (softDrink == 'S'){
+				System.out.println("Collect Sprite");
+			} else if (softDrink == 'F'){
+				System.out.println("Collect fanta");
+			}
+		}
+	}
+}
