819807fc5ef1e8e1d25fca46bcd2aecba4cb9e4c
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-03-27 11:03:45 +0530

    Create if-else.java

---
+++ if-else.java
@@ -0,0 +1,14 @@
+class hemat
+{
+	public static void main(String []args)
+	{
+		int token = 40;
+		if (token == 30){
+			System.out.println("Collect token");
+		} else if(token == 20){
+			System.out.println("Collect a token ");
+		} else{
+			System.out.println("Invalid token");
+		}
+	}
+}
