ca3fd7ef5df68fb71369bb73f3380151e5d58f59
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-03-28 12:30:04 +0530

    Create oddeven.java

---
+++ oddeven.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+        int remainder = 4 % 3;
+        switch (remainder) {
+            case 0:
+                System.out.println("Even Number");
+                break;
+
+            case 1:
+                System.out.println("Odd Number");
+                break;
+        }
+    }
+}
